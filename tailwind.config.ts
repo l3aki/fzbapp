b2cf8d998202a5da79b87752870df1328023d1da
baki <bakikaradeniz@gmail.com>
2025-04-02 01:21:26 +0300

    Add Tailwind configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      },
+      colors: {
+        'vibe-blue': '#1E40AF',
+        'vibe-orange': '#F97316',
+        'vibe-yellow': '#FBBF24',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
